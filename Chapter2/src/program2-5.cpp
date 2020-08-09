1a23f853f9200dde24b9b01ea28e5172a9a1aec4
Zachary Moote <nuluumo@gmail.com>
2020-08-09 19:49:53 -0400

    22.22% Complete with Chapter 2

---
+++ Chapter2/src/program2-5.cpp
@@ -0,0 +1,13 @@
+// A well-adjusted printing program
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "The following items were top sellers" << endl;
+    cout << "during the month of June:" << endl;
+    cout << "Computer games" << endl;
+    cout << "Coffee" << endl;
+    cout << "Asprin" << endl;
+    return 0;
+}
